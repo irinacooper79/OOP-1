ced9b8637c662d471f601030a38f5703fe63a36b
Дом <ira-rostov80@mail.ru>
2022-10-25 21:59:02 +0300

    первый коммит-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+
+        Human maxim = new Human() {
+            maxim.name ="Максим";
+            maxim.yearOfBirth =1985;
+            maxim.town ="Минск";
+            maxim.jobTitle ="Бренд-менеджер";
+        System.out.println(maxim);
+
+            Human anya = new Human() {
+                anya.name ="Аня";
+                anya.yearOfBirth =1993;
+                anya.town =" Москва";
+                anya.jobTitle ="Методист";
+        System.out.println(anya);
+
+                Human katay = new Human() {
+                    katay.name ="Катя";
+                    katay.yearOfBirth =1994;
+                    katay.town ="Калининград";
+                    katay.jobTitle ="Продакт-менеджер";
+        System.out.println(katay);
+
+                    Human artem = new Human() {
+                        artem.name ="Артем";
+                        artem.yearOfBirth =1995;
+                        artem.town ="Москва";
+                        artem.jobTitle ="Директор по развитию бизнеса";
+        System.out.println(artem);
+                    }
+
+
+     Car Kia = new Car
+             ("Kia", "Sportage", "2.4f", "Red", "2018", "South Korea");
+          System.out.println(Kia);
+           }
+           }
---
+++ src/ValidationUtils.java
@@ -0,0 +1,7 @@
+public class ValidationUtils {
+
+    public static String validOrDefault( String value, String defaultvalue){
+        return value == null || value.isBlank();
+        defaultValue : value;
+    }
+}
